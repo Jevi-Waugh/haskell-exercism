81890b157cf4d94d113f8ed652aba083e7544d6f
Jevi Waugh <codeprogramming99@gmail.com>
2026-01-06 14:47:51 +1000

    Added ds folder and Binary tree

---
+++ data-structures/BinaryTree.hs
@@ -0,0 +1,38 @@
+module BinaryTree where
+
+data Tree a  = Empty | Node (Tree a) a (Tree a) deriving Show
+
+-- inserts an element into a bst
+insert :: Ord a => a -> Tree a -> Tree a
+insert num (Empty) = Node (Empty) num (Empty)
+insert num (Node left value right)
+        -- left value - greater values
+        | value < num                    = Node left value (insert num right)
+        -- right value - lesser values
+        | value >= num                    = Node (insert num left) value right
+        -- equal value
+        -- | x == num                       = 
+
+-- finds an element in a bst
+find :: Ord a => a -> Tree a -> Maybe a
+find _ Empty = Nothing
+find num (Node left value right)
+    | value == num = Just num
+    | value < num = find num right
+    | value >= num = find num left
+
+-- -- mirror image of a tree
+mirror :: Tree a -> Tree a
+mirror Empty = Empty
+mirror (Node (left) value (right)) = Node (mirror right) value (mirror left)
+
+
+
+
+
+
+
+
+
+
+

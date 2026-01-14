ac21ddd47e2be1508effc5579c9e518ea6d46811
Jevi Waugh <codeprogramming99@gmail.com>
2026-01-14 21:59:43 +1000

    Partial solution

--- Series.hs
+++ Series.hs
@@ -1,9 +1,25 @@
 module Series (slices) where
 
+
+
 slices :: Int -> String -> [[Int]]
-slices n xs
-    | n >= length xs        = read xss :: [Int]
+slices 0 [] = [[]]
+
+slices n (x:xs)
+    | n >= length (x:xs)        = [typeCastAll (x:xs)]-- [[read (x:xs) :: Int]]
+    |otherwise                  = this
+    -- take n (x:xs)
 
     where
-        xss = foldr f [] xs
-        f = 
+        rest = typeCastAll (x:xs)
+        this = [take n rest] ++ slices n (tail xs)
+
+
+numTup :: [(Char, Int)]
+numTup  = zip ['0'..'9'] [0..9]
+
+typeCast :: Char -> Int
+typeCast number = head [i | (s,i) <- numTup, s == number]
+
+typeCastAll :: String -> [Int]
+typeCastAll = map typeCast 

6a80bdca23151361d908a8409a9edc53045e582b
Jevi Waugh <codeprogramming99@gmail.com>
2026-01-14 22:03:51 +1000

    Fixed one bug -removed tail function

--- Series.hs
+++ Series.hs
@@ -1,9 +1,10 @@
 module Series (slices) where
 
-
+-- Example -> slices 3 "49142"
+-- [[4,9,1],[9,1,4],[1,4,2]]
 
 slices :: Int -> String -> [[Int]]
-slices 0 [] = [[]]
+slices 0 _ = [[]]
 
 slices n (x:xs)
     | n >= length (x:xs)        = [typeCastAll (x:xs)]-- [[read (x:xs) :: Int]]
@@ -11,8 +12,9 @@ slices n (x:xs)
     -- take n (x:xs)
 
     where
+        -- Typecast to integers
         rest = typeCastAll (x:xs)
-        this = [take n rest] ++ slices n (tail xs)
+        this = [take n rest] ++ slices n xs
 
 
 numTup :: [(Char, Int)]

531b7e89ee79083fd34b244696dae306db28d121
Jevi Waugh <codeprogramming99@gmail.com>
2026-01-15 16:01:41 +1000

    Solution updated

--- Triplet.hs
+++ Triplet.hs
@@ -1,9 +1,21 @@
 module Triplet (tripletsWithSum) where
 
+-- Inefficient solution
+-- tripletsWithSum :: Int -> [(Int, Int, Int)]
+-- tripletsWithSum n = [(x,y,z) | x <- [1..n], y <- [x+1..n], z <- [y+1..n], theorem x y z n]
+
+-- theorem :: Int -> Int -> Int -> Int -> Bool
+-- theorem x y z n = if (x^2) + (y^2) == (z^2) && x + y + z == n then True else False
+
+
+-- Paul's solution
 tripletsWithSum :: Int -> [(Int, Int, Int)]
-tripletsWithSum n = [(x,y,z) | x <- [1..n], y <- [x+1..n], z <- [y+1..n], theorem x y z n]
-
-
-theorem :: Int -> Int -> Int -> Int -> Bool
-theorem x y z n = if (x^2) + (y^2) == (z^2) && x + y + z == n then True else False
+tripletsWithSum n = 
+    [(x,y,z) |
+    z <- [1..n], 
+    y <- [1..n], 
+    x <- [n - y - z], 
+    x > 0,
+    x < y,
+    (z^2 - y^2) == (x^2)]
 
